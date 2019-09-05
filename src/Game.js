0e56014aeedccab0dbd56ae76147889aafa51e68
Rebecca Park <burugirl93@gmail.com>
2019-09-05 16:30:07 +0900

    feat/game/can-populate-a-fixed-number-of-mines-randomly

--- src/Game.js
+++ src/Game.js
@@ -1,25 +1,47 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Game.css';
 
-const HEIGHT = 20;
-const WIDTH = 15;
-const NUM_MINE = 10;
+const HEIGHT = 5;
+const WIDTH = 2;
+const NUM_MINE = 5;
 
 function Game() {
-  const [board, setBoard] = useState(Array.from(
-    Array(HEIGHT), () =>
-    new Array(WIDTH).fill({
-      hasMine: false,
-      unCovered: false,
-      numMinesAround: 0,
-    })
-    ));
+  const [board, setBoard] = useState([]);
 
-  function populateBoard() {
+  useEffect(() => {
+    function populateMines() {
+      let initialBoard = Array.from(
+        Array(HEIGHT), () =>
+        new Array(WIDTH).fill({
+          hasMine: false,
+          isUncovered: false,
+          numMinesAround: 0,
+        })
+        );
 
-  }
-  
-  return(<div>Hello</div>);
+      let numInjectedMines = 0;
+      let tempBoard = JSON.parse(JSON.stringify(initialBoard));
+
+      for(let i=0; i < initialBoard.length; i++) {
+        for(let j=0; j < initialBoard[i].length; j++) {
+          const shoudInjectMine = Math.floor(Math.random() * Math.floor(2));
+
+          if (shoudInjectMine === 1 && numInjectedMines < NUM_MINE) {
+            tempBoard[i][j].hasMine = true;
+            numInjectedMines = numInjectedMines + 1;
+          }
+        }
+      }
+      setBoard(tempBoard);
+    }
+    populateMines();
+  }, []);
+
+  return(
+    <div>
+      Welcome to minesweeper!
+    </div>
+  );
 }
 
 export default Game;
