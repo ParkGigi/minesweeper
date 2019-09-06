9f4dd1cf88342ade58c3e41e80975ce799cddda0
Rebecca Park <burugirl93@gmail.com>
2019-09-06 15:53:22 +0900

    feat/constants/added game constants

--- src/Game.js
+++ src/Game.js
@@ -1,53 +1,58 @@
 import React, { useState, useEffect } from 'react';
 import './Game.css';
 
-const HEIGHT = 5;
-const WIDTH = 2;
-const NUM_MINE = 8;
+import { Board } from './components/Board';
+
+import { ROWS, COLUMNS, NUM_MINE, InjectMineEnum } from './constants/gameConstants';
 
 function Game() {
   const [board, setBoard] = useState([]);
 
   useEffect(() => {
-    function populateMines() {
-      let initialBoard = Array.from(
-        Array(HEIGHT), () =>
-        new Array(WIDTH).fill({
-          hasMine: false,
-          isUncovered: false,
-          numMinesAround: 0,
-        })
-        );
-
-      let numInjectedMines = 0;
-      let tempBoard = JSON.parse(JSON.stringify(initialBoard));
-
-      let rowIndex = 0;
-
-      while(numInjectedMines < NUM_MINE) {
-        for(let i=0; i < initialBoard[rowIndex].length; i++) {
-          const shouldInjectMine = Math.floor(Math.random() * Math.floor(2));
-
-          if(shouldInjectMine === 1 && tempBoard[rowIndex][i].hasMine === false) {
-            tempBoard[rowIndex][i].hasMine = true;
-            numInjectedMines++;
-          }
-        }
-        
-        if (rowIndex === initialBoard.length - 1) {
-          rowIndex = 0;
-        } else {
-          rowIndex++;
-        }
-      }
-      setBoard(tempBoard);
-    }
     populateMines();
   }, []);
 
+  function initiateEmptyBoard() {
+    return Array.from(
+      Array(ROWS), () =>
+      new Array(COLUMNS).fill({
+        hasMine: false,
+        isUncovered: false,
+        numMinesAround: 0,
+      })
+      );
+  }
+
+  function populateMines() {
+    let emptyBoard = initiateEmptyBoard();
+
+    let numInjectedMines = 0;
+    let tempBoard = JSON.parse(JSON.stringify(emptyBoard));
+
+    let rowIndex = 0;
+
+    while(numInjectedMines < NUM_MINE) {
+      for(let i=0; i < emptyBoard[rowIndex].length; i++) {
+        const shouldInjectMine = Math.floor(Math.random() * Math.floor(Object.keys(InjectMineEnum).length));
+
+        if(shouldInjectMine === InjectMineEnum.INJECT && tempBoard[rowIndex][i].hasMine === false) {
+          tempBoard[rowIndex][i].hasMine = true;
+          numInjectedMines++;
+        }
+      }
+      
+      if (rowIndex === emptyBoard.length - 1) {
+        rowIndex = 0;
+      } else {
+        rowIndex++;
+      }
+    }
+    setBoard(tempBoard);
+  }
+
   return(
     <div>
-      Welcome to minesweeper!
+      <Board board={board}/>
     </div>
   );
 }
---
+++ src/components/Board.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Board.scss';
+
+export function Board(props) {
+  console.log('board: ', props.board);
+  return(
+    <div 
+      className="board">
+      board
+    </div>);
+}
